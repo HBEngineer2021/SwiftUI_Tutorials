a843375149d49acfcc0684faf8fe8e7475bc926c
Hanagasaki <stze961q@gmail.com>
2022-02-24 22:28:47 +0900

    SwiftUI Tutorialsを見ながら、SwiftUIを学習する①

---
+++ SwiftUI_Tutorial/SwiftUI_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialApp.swift
+//  SwiftUI_Tutorial
+//
+//  Created by Motoki Onayama on 2022/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI_Tutorial/TextView.swift
@@ -0,0 +1,48 @@
+//
+//  TextView.swift
+//  SwiftUI_Tutorial
+//
+//  Created by Motoki Onayama on 2022/02/23.
+//
+
+import SwiftUI
+
+struct TextView: View {
+    
+    @State var btnLbl = "Button"
+    @State var btnLbl1 = "Button1"
+    var body: some View {
+        VStack {
+            VStack(alignment: .leading) {
+                Text("Hello, world!")
+                    .font(.title)
+                    .foregroundColor(Color.red)
+                Text("Hello, world!")
+                Button(action: {
+                    btnLbl = "Hello, world!!!!"
+                }, label: {
+                    Text(btnLbl)
+                })
+            }
+            .padding()
+            VStack(alignment: .trailing) {
+                Text("Hello, world!")
+                    .font(.largeTitle)
+                    .foregroundColor(Color.yellow)
+                Text("Hello, world!")
+                Button(action: {
+                    btnLbl1 = "Hello, world!!!!"
+                }, label: {
+                    Text(btnLbl1)
+                })
+            }
+            .padding()
+        }
+    }
+}
+
+struct TextView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextView()
+    }
+}
