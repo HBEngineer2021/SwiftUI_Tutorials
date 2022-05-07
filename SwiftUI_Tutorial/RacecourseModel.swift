ea5340090fc2d3467d8c291e8ee5e69d6fb848b8
Hanagasaki <stze961q@gmail.com>
2022-05-07 17:36:25 +0900

    SwiftUI Tutorialsを見ながら、SwiftUIを学習する⑥

---
+++ SwiftUI_Tutorial/RacecourseModel.swift
@@ -0,0 +1,14 @@
+//
+//  RacecourseModel.swift
+//  SwiftUI_Tutorial
+//
+//  Created by Motoki Onayama on 2022/03/06.
+//
+
+import Foundation
+import SwiftUI
+
+struct RacecourseModel {
+    var name: String
+    var imageName: String
+}
